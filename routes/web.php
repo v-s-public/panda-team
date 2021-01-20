26d9bf06418461575723c42797c806b66094a221
Vo Saan <vo.saan@gmail.com>
2021-01-20 13:55:07 +0200

    division into frontend and admin parts

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Frontend
+Route::domain(env('APP_DOMAIN'))->group(function () {
+    Route::resource('/', App\Http\Controllers\Frontend\TasksController::class);
 });
+
+// Auth
+Route::get('login', [App\Http\Controllers\Auth\LoginController::class, 'showLoginForm'])->name('login');
+Route::post('login', [App\Http\Controllers\Auth\LoginController::class, 'login']);
+Route::post('logout', [App\Http\Controllers\Auth\LoginController::class, 'logout'])->name('logout');
+
+// Backend
+Route::domain('admin.' . env('APP_DOMAIN'))->group(function () {
+    Route::middleware('auth')->name('admin.')->group(function () {
+        Route::resource('/', App\Http\Controllers\Admin\TasksController::class);
+    });
+});
+
+
